3c517dc9575869be79fdc5dac855327d77690900
Edith Oga <83961446+vigehi@users.noreply.github.com>
2023-02-02 16:45:36 -0300

    fix all linters and minor fixes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import Doctors from './components/doctors/Doctors';
 import DoctorNew from './components/doctors/doctor-new/DoctorNew';
 import DoctorEdit from './components/doctors/doctor-edit/DoctorEdit';
 
-
-
 const App = () => (
   <Router>
     <Routes>
